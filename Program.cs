69537b972c0a0e63324140f6090fbd98da4437f9
tomrosso <trosso2024@chadwickschool.org>
2022-01-20 12:07:20 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NFL_QBR_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
