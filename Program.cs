97af5449a8e0042dd1860490c02114364680d400
tomrosso <trosso2024@chadwickschool.org>
2022-01-25 10:37:29 -0800

    Worked in class

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace NFL_QBR_Tracker
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void year(string year)
         {
-            Console.WriteLine("Hello World!");
+    // 1. Check if filename exists`
+    // 2. if false throw exception "choose a year between 1980 and 2020"
+    // 3. if true construct a list <int> named years to store the years
+    // 4. Load "years.txt" file and store it in a variable: years File.Readlines ("years.txt")
+    // 5. Return new: teams()
+        }
+
+        public static void team(int year, string team)
+        {
+
+        }
+        public static void stats(int year, string team, string stat)
+        {
+
         }
     }
 }
+
