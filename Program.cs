25f698f669fa42a70c9a813106a8eb60c0a03615
tomrosso <trosso2024@chadwickschool.org>
2022-01-27 00:09:13 -0800

    feat: Add method stubs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ namespace NFL_QBR_Tracker
 {
     class Program
     {
-        public static void years(List<int> values, string year)
+        
+        /// <summary>
+        /// Given a prompt to show the user, displays the prompt then reads
+        /// input from keyboard until user types in a year displayed.
+        /// If the user does not enter a year displayed the message displays an error
+        /// message and prompts the user again.
+        /// </summary>
+        /// <param name="year">The number between 1980 and 2020 the user chose</param>
+        public static void years(string year)
         {
             // 1. Check if filename exists`
             // 2. if false throw exception "choose a year between 1980 and 2020"
@@ -12,8 +20,16 @@ namespace NFL_QBR_Tracker
             // 4. Load "years.txt" file and store it in a variable: years File.Readlines ("years.txt")
             // 5. Return new: teams()
         }
-
-        public static void team(list<int> values, int year, string team)
+        
+        /// <summary>
+        /// Given a prompt to show the user, displays the prompt then reads
+        /// input from keyboard until user types in a team displayed.
+        /// If the user does not enter a team displayed the message displays an error
+        /// message and prompts the user again.
+        /// </summary>
+        /// <param name="year">The number between 1980 and 2020 the user chose</param>
+        /// <param name="team">The choices to display to the user chose</param>
+        public static void team(int year, string team)
         {
             // 1. Check if filename exists`
             // 2. if false throw exception "choose a team from the year you have chosen"
@@ -21,7 +37,17 @@ namespace NFL_QBR_Tracker
             // 4. Load "teams.txt" file and store it in a variable: teams File.Readlines ("teams.txt")
             // 5. Return new: stats()
         }
-        public static void stats(list<int> values, int year, string team, string stat)
+
+        /// <summary>
+        /// Given a prompt to show the user, displays the prompt then reads
+        /// input from keyboard until user types in a QB stat displayed.
+        /// If the user does not enter a QB stat displayed the message displays an error
+        /// message and prompts the user again.
+        /// </summary>
+        /// <param name="year">The number between 1980 and 2020 the user chose</param>
+        /// <param name="team">The choices to display to the user chose</param>
+        /// <param name="stat">The choices to display to the user chose</param>
+        public static void stats(int year, string team, string stat)
         {
             // 1. Check if filename exists`
             // 2. if false throw exception "Choose a quarterback stat listed"
