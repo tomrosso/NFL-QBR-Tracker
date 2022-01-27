817538a2bd58f449dce55ada67ee8d995258abe0
tomrosso <trosso2024@chadwickschool.org>
2022-01-26 23:50:40 -0800

    Worked on stubs

--- Program.cs
+++ Program.cs
@@ -4,22 +4,30 @@ namespace NFL_QBR_Tracker
 {
     class Program
     {
-        public static void year(string year)
+        public static void years(List<int> values, string year)
         {
-    // 1. Check if filename exists`
-    // 2. if false throw exception "choose a year between 1980 and 2020"
-    // 3. if true construct a list <int> named years to store the years
-    // 4. Load "years.txt" file and store it in a variable: years File.Readlines ("years.txt")
-    // 5. Return new: teams()
+            // 1. Check if filename exists`
+            // 2. if false throw exception "choose a year between 1980 and 2020"
+            // 3. if true construct a list <int> named years to store the years
+            // 4. Load "years.txt" file and store it in a variable: years File.Readlines ("years.txt")
+            // 5. Return new: teams()
         }
 
-        public static void team(int year, string team)
+        public static void team(list<int> values, int year, string team)
         {
-
+            // 1. Check if filename exists`
+            // 2. if false throw exception "choose a team from the year you have chosen"
+            // 3. if true construct a list <int> named teams to store the teams
+            // 4. Load "teams.txt" file and store it in a variable: teams File.Readlines ("teams.txt")
+            // 5. Return new: stats()
         }
-        public static void stats(int year, string team, string stat)
+        public static void stats(list<int> values, int year, string team, string stat)
         {
-
+            // 1. Check if filename exists`
+            // 2. if false throw exception "Choose a quarterback stat listed"
+            // 3. if true construct a list <int> named stats to store the stats
+            // 4. Load "stats.txt" file and store it in a variable: stats File.Readlines ("stats.txt")
+            // 5. Return: years()
         }
     }
 }
