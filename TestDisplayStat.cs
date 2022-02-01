4e242b6995886cf4d72a6512bfdf1b7436b6fb40
tomrosso <trosso2024@chadwickschool.org>
2022-02-01 10:36:21 -0800

    Testing Files in class

--- TestDisplayStat.cs
+++ TestDisplayStat.cs
@@ -9,6 +9,9 @@ namespace NFL_QBR_Tracker
 
 public static bool RunTest()
 {
+    //         public static void DisplayStat(string year, string team, string stat)
+Console.WriteLine("Running DisplayStat(string year, string team, string stat). It should produce multiple messages asking you to enter a Year, Team and a QB stat.");
+    Console.WriteLine("Rams, 1992, and then Total passing yds. It should give you 3218 yds.");
     return false;
 }
    
--- TestGetStat.cs
+++ TestGetStat.cs
@@ -11,18 +11,23 @@ public static bool RunTest()
 {
     Console.WriteLine("Running GetStat(List<string> validStats). It should produce a message asking you to enter a QB stat.");
     Console.WriteLine("Enter Shots Per Game, then tackles, and then QBR. Should say invalid, invalid, valid.");
-     // TODO(jcollard 2022-02-01): You should rewrite this as:
-    //  List<string> validStats = new List<string>();
-    //  validStats.Add("QBR");
-    //  validStats.Add("???");
-    // Add more stats
-    // Then finally call GetTeam with the list
-    //  string result = Program.GetTeam(validStats);
 
-    string result = Program.GetTeam(List<string> validStats);
-    if (result != "QBR")
+  
+    List<string> validStats = new List<string>();
     {
-        return false;
+    validStats.Add("QBR");
+    validStats.Add("Rushing yds per game");
+    validStats.Add("Passing yds per game");
+    validStats.Add("INTs per game");
+    validStats.Add("Completion % per game");
+    validStats.Add("Pass attempts per game");
+    validStats.Add("Touchdowns per game");
+    validStats.Add("Total touchdowns");
+    validStats.Add("Total passing yds");
+    validStats.Add("Total rushing yds");
+    validStats.Add("Total INTs");
+    validStats.Add("Total pass attempts");
+    string result = Program.GetStat(validStats);
     }
     
     return true;
