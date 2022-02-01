3557a889bce8237039c8027aa0226b92c8fc9214
tomrosso <trosso2024@chadwickschool.org>
2022-01-31 22:02:43 -0800

    Worked for HW

--- TestGetStat.cs
+++ TestGetStat.cs
@@ -9,7 +9,15 @@ namespace NFL_QBR_Tracker
 
 public static bool RunTest()
 {
-    return false;
+    Console.WriteLine("Running GetStat(List<string> validStats). It should produce a message asking you to enter a QB stat.");
+    Console.WriteLine("Enter Shots Per Game, then tackles, and then QBR. Should say invalid, invalid, valid.");
+    string result = Program.GetTeam(List<string> validStats);
+    if (result != "QBR")
+    {
+        return false;
+    }
+    
+    return true;
 }
    
     }
--- TestGetTeam.cs
+++ TestGetTeam.cs
@@ -9,7 +9,15 @@ namespace NFL_QBR_Tracker
 
 public static bool RunTest()
 {
-    return false;
+    Console.WriteLine("Running GetTeam(List<string> validTeams). It should produce a message asking you to enter a team.");
+    Console.WriteLine("Enter Tigers, then Cubs, and then Patriots. Should say invalid, invalid, valid.");
+    string result = Program.GetTeam(List<string> validTeams);
+    if (result != "Patriots")
+    {
+        return false;
+    }
+    
+    return true;
 }
    
     }
